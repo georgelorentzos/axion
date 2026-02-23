ec0aa88bb6a7e1c686d7b22d9eb07444a32c0440
George Lorentzos <lorentzos@Mac.home>
2026-02-23 19:22:42 +0200

    fix: minor bug fixes

--- frontend/src/components/common/UserCard.tsx
+++ frontend/src/components/common/UserCard.tsx
@@ -291,8 +291,8 @@ export default function UserCard({
 
   return (
     <div
-      onClick={currentUser ? () => {} : handleNavigateToChat}
-      className={` ${currentUser ? "cursor-default" : "cursor-pointer"} transition duration-300 h-[60px] px-4 flex justify-between items-center w-full rounded-lg ${
+      onClick={currentUser?.id === user?.id ? () => {} : handleNavigateToChat}
+      className={` ${currentUser?.id === user?.id ? "cursor-default" : "cursor-pointer"} transition duration-300 h-[60px] px-4 flex justify-between items-center w-full rounded-lg ${
     isSelected
       ? "bg-basalt"
       : "hover:bg-basalt"
